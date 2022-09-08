28a675feeeedf05779d967b2e110baa9bfdab747
w1ldweasel <46282239+w1ldweasel@users.noreply.github.com>
2022-09-08 23:44:03 +0000

    new app

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+var express = require('express');
 const { connected } = require('process');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
+io.on('connection', function(client){
+  console.log(Client connected...'');
 
 <<<<<<< Updated upstream
 const server = http.createServer((req, res) => {
@@ -10,5 +16,8 @@ const server = http.createServer((req, res) => {
 >>>>>>> Stashed changes
 });
 
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
+server.listen(8080);
