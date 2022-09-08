a544ab185806d1b5b7a924e576b366873c626d3b
w1ldweasel <46282239+w1ldweasel@users.noreply.github.com>
2022-09-08 23:49:40 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
 var express = require('express');
-const { connected } = require('process');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 io.on('connection', function(client){
   console.log(Client connected...'');
-
-<<<<<<< Updated upstream
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-=======
->>>>>>> Stashed changes
 });
 
 app.get('/', function (req, res){
