7a222b80c0039d3a43c9f5d9b6b0979bcac4484f
w1ldweasel <46282239+w1ldweasel@users.noreply.github.com>
2022-09-14 16:01:27 +0000

    socket io node chat app

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ io.on('connection', function(client){
   console.log('Client connected...');
 
   client.emit('messages', {hello: 'world'});
+
+  client.on('messages', function(data){
+    console.log(data);
+  });
 });
 
 app.get('/', function (req, res){
