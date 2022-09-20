6a43718d25f1f96922bc7ec225317f826802f982
w1ldweasel <46282239+w1ldweasel@users.noreply.github.com>
2022-09-20 14:28:51 +0000

    Update app.js

--- NodeApp1/app.js
+++ NodeApp1/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var hc_password = "mytestpassword";
 
 io.on('connection', function(client){
   console.log('Client connected...');
