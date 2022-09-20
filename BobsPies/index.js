3c1e50c8ebe3f06ab72c9cc53ba41a5ea6f4cbf8
w1ldweasel <46282239+w1ldweasel@users.noreply.github.com>
2022-09-20 16:13:40 +0000

    Update index.js

--- BobsPies/index.js
+++ BobsPies/index.js
@@ -7,12 +7,19 @@ let router = express.Router();
 let pies = [
     { "id":1, "name": "Apple"},
     { "id":2, "name": "Cherry"},
-    { "id":3, "name": "Peach"}
+    { "id":3, "name": "Peach"},
+    { "id":4, "name": "Pear"}
 ];
 let hc_password = "mytestpassword";
 //Create GET to return a list of all pies
 router.get('/', function(req, res, next){
-    res.status(206).send(pies);
+    res.status(200).json({
+        "status": 200,
+        "statusText": "OK",
+        "message": "All pies retrieved.",
+        "data": pies
+
+    });
 });
 
 //Configure router so all routes are prefixed with /api/v1
