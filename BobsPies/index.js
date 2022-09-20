fe9a84769d8dfea8bc28ecc6ed60e800811a285e
w1ldweasel <46282239+w1ldweasel@users.noreply.github.com>
2022-09-20 01:21:33 +0000

    New pie proj

---
+++ BobsPies/index.js
@@ -0,0 +1,20 @@
+// Bring ing express server and create app
+let express = require('express');
+let app = express();
+
+//Use the express Router object
+let router = express.Router();
+
+//Create GET to return a list of all pies
+router.get('/', function(req, res, next){
+    res.send("Apple");
+});
+
+//Configure router so all routes are prefixed with /api/v1
+app.use('/api/', router);
+
+//Create server to listen on port 5000
+var server = app.listen(5000, function(){
+    console.log('Node server is running on http://localhost:5000');
+})
+
