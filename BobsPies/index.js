02c62729885344549699af5e4ce800a7531b865d
w1ldweasel <46282239+w1ldweasel@users.noreply.github.com>
2022-10-04 01:06:53 +0000

    Added retrieve by id function

--- BobsPies/index.js
+++ BobsPies/index.js
@@ -19,6 +19,34 @@ router.get('/', function(req, res, next){
     }, function(err){
         next(err);
     });
+
+});
+
+    //Create GET/id to return a single pie
+router.get('/:id', function(req, res, next){
+    pieRepo.getById(req.params.id, function(data) {
+        if (data) {
+            res.status(200).json({
+                "status": 200,
+                "statusText": "OK",
+                "message": "Single pie retrieved.",
+                "data": data
+            });
+        }
+        else {
+        res.status(404).json({
+            "status": 404,
+            "statusText": "Not Found",
+            "message": "The pie '" + req.params.id + "' could not be retrieved.", 
+            "error": {
+                "code": "NOT_FOUND",
+                "message": "The pie '" + req.params.id + "' could not be retrieved.", 
+            }
+        });
+    }
+}, function(err) {
+    next(err);
+});
 });
 
 //Configure router so all routes are prefixed with /api/v1
@@ -30,4 +58,3 @@ var server = app.listen(5000, function(){
 console.log('Node server is running on http://localhost:5000');
 console.log('Node server is running on http://localhost:5000');
 })
-
